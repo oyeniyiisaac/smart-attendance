c63b0aca84670964ae250fac1b2b988f2bf397e0
Oyeniyi Isaac <isaacoyeniyi6@gmail.com>
2026-07-25 11:55:15 +0100

    feat: add Progressive Web App support using vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'Smart Attendance System',
+        short_name: 'SmartAttendance',
+        description: 'Institutional Attendance Tracking System',
+        theme_color: '#0a643a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
+    },
+    devOptions: {
+        enabled: true
+      }
+  })],
 })
